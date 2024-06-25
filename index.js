3644679aef8846fc20f6ca3ecd3c6b496d3ad2c1
Shruti Tirpude <Shruti.Tirpude@bp.com>
2024-06-25 14:59:01 -0500

    git add log for rebase testing in master

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log("This is a nodeJs program");
 console.log("The change is in master branch");
 console.log("Add a new change");
 console.log("Add a a new change to test merge logs after branches are merged");
+console.log("Add a a new change to test merge logs after branches are rebased");
